e7b64c1891997bc8a4045641a9583200e5c3e821
Rob Kenis <robke@softwareag.com>
2023-09-29 14:08:22 +0200

    Optimize imports

--- falco-webhook/app.py
+++ falco-webhook/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
 import logging
-from kubernetes import client, config
+
+from flask import Flask, request
+from kubernetes import client
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
