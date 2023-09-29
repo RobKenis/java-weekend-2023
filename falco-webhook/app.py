effae952febf4d4ea5b5030c3663cd1589c437b0
Rob Kenis <robke@softwareag.com>
2023-09-29 14:04:29 +0200

    Remove label from pod when tcpdump

--- falco-webhook/app.py
+++ falco-webhook/app.py
@@ -1,13 +1,22 @@
 from flask import Flask, request
 import logging
+from kubernetes import client, config
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
+
+config.load_kube_config()
+v1 = client.CoreV1Api()
 
 
 @app.get("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+def _remove_allow_ingress_label(pod, namespace):
+    app.logger.info(f"Removing label from pod [{pod}] in namespace [{namespace}]")
+    v1.patch_namespaced_pod(pod, namespace, {"metadata": {"labels": {"allow-ingress-to": "none"}}})
 
 
 @app.post("/")
@@ -17,4 +26,6 @@ def falco():
     if body['priority'] == 'Warning' and body['rule'] == 'do_not_tcpdump':
         app.logger.info(f"TCPDUMP detected in [{body['output_fields']['k8s.pod.name']}] "
                         f"in namespace [{body['output_fields']['k8s.ns.name']}]")
+        _remove_allow_ingress_label(body['output_fields']['k8s.pod.name'],
+                                    body['output_fields']['k8s.ns.name'])
     return "OK"
